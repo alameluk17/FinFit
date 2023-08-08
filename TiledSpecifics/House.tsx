e24bd18677610d942f41c8d0f83d98706c364164
Jahnavi Murali <109456718+jahnavimurali@users.noreply.github.com>
2023-08-09 01:22:05 +0530

    Sprite sheets for buildings and landscape added

---
+++ TiledSpecifics/House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="House" tilewidth="64" tileheight="64" tilecount="72" columns="8">
+ <image source="../Assets/House.png" width="512" height="576"/>
+</tileset>
