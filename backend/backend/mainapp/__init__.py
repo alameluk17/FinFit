7be4e2b764266e9915ac1b64e477cd0b9d1bf72e
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-08-09 17:18:26 +0530

    Add required models.

---
+++ backend/backend/mainapp/__init__.py
@@ -0,0 +1,20 @@
+from string import ascii_uppercase, digits
+
+# Some Game Constants:
+class GAME_CONSTANTS:
+    STARTING_ACCOUNT_BALANCE= 10000
+    STARTING_MONTHLY_EXPENSES= 10000
+    STARTING_MONTHLY_SALARY= 25000
+    STARTING_KINDESS_INDEX= 20
+    GOVERNMENT_ID_LENGTH= 12
+    GOVERNMENT_ID_ALPHABET= ascii_uppercase + digits
+    ACCOUNT_LOCATIONS= [
+        ("PRB" ,"Private Bank"),
+        ("PBB" ,"Public Bank"),
+        ("PO" ,"Post Office")
+    ]
+    ASSET_TYPES = [
+        ("AP", "Apartment"),
+        ("IH", "Individual House"),
+        ("PL", "Plot"),
+    ]
--- backend/backend/mainapp/models.py
+++ backend/backend/mainapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+from . import GAME_CONSTANTS
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    account_location = models.CharField(max_length=3,choices = GAME_CONSTANTS.ACCOUNT_LOCATIONS,null=False)
+    account_balance = models.FloatField(default=GAME_CONSTANTS.STARTING_ACCOUNT_BALANCE,null=False)
+    monthly_expenses = models.BigIntegerField(default=GAME_CONSTANTS.STARTING_MONTHLY_EXPENSES,null=False)
+    monthly_salary = models.BigIntegerField(default=GAME_CONSTANTS.STARTING_MONTHLY_SALARY,null=False)
+    government_id = models.CharField(max_length=12)
+    net_worth = models.BigIntegerField(default=0,null=False)
+    kindness_index = models.PositiveSmallIntegerField(max_length=2,null=False,default=GAME_CONSTANTS.STARTING_KINDESS_INDEX)
+
+class FixedDeposit(models.Model):
+    principal = models.FloatField(null=False)
+    interest = models.FloatField(null=False)
+    term = models.IntegerField(null=False)
+    start_date = models.DateField(null=False,auto_now_add=True)
+    owner = models.ForeignKey(Player)
+    location =  models.CharField(max_length=3,choices = GAME_CONSTANTS.ACCOUNT_LOCATIONS)
+
+class Asset(models.Model):
+    owner = models.ForeignKey(Player,null=False)
+    asset_type = models.CharField(max_length=3,choices=GAME_CONSTANTS.ASSET_TYPES,null=False)
+    start_date = models.DateField(auto_now_add=True,null=False)
+    buying_price = models.FloatField(null=False)
+    value = models.FloatField(null=False)
+    location =  models.CharField(max_length=3,choices = GAME_CONSTANTS.ACCOUNT_LOCATIONS)
+
+class CharityTransactions(models.Model):
+    depositor = models.ForeignKey(Player,null=False)
+    amount = models.FloatField(null=False)
+    date = models.DateField(auto_now_add=True,null=False)
