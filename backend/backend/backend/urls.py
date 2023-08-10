cb061499e296088961e834b9c5c029c20f64e2cc
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-08-10 18:01:00 +0530

    Add admin page to urls.

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.urls import include, path
 from rest_framework import routers
+from django.contrib import admin
 from mainapp import views
 
 router = routers.DefaultRouter()
@@ -26,6 +27,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
