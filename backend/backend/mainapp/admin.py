c32f36ca6773dbce4f2dd8839cf5fab1af9d4177
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-08-10 18:28:05 +0530

    Register models with admin page

--- backend/backend/mainapp/admin.py
+++ backend/backend/mainapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Player
+from .models import Player,FixedDeposit,Asset,CharityTransaction
 
 admin.site.register(Player)
+admin.site.register(FixedDeposit)
+admin.site.register(Asset)
+admin.site.register(CharityTransaction)
