82788a6e5041d35f9edbc148215a863b7ae784a6
Jahnavi Murali <109456718+jahnavimurali@users.noreply.github.com>
2023-08-15 11:16:31 +0530

    completed tileMap, added Assets

---
+++ TiledSpecifics/Park.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Park" tilewidth="64" tileheight="64" tilecount="16" columns="8">
+ <image source="../Assets/Park.png" width="512" height="128"/>
+</tileset>
