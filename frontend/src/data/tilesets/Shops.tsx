f1625b68849437f21ceb318ead8d0caf8e050470
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-08-18 16:35:55 +0530

    Moved tilesets to appropriate folder.

--- frontend/src/data/tilesets/Shops.tsx
+++ frontend/src/data/tilesets/Shops.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Shops" tilewidth="64" tileheight="64" tilecount="66" columns="22">
- <image source="../frontend/src/data/img/spritesheets/Shops.png" width="1408" height="192"/>
+ <image source="../img/spritesheets/Shops.png" width="1408" height="192"/>
 </tileset>
--- frontend/src/data/tilesets/Trees.tsx
+++ frontend/src/data/tilesets/Trees.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Trees" tilewidth="64" tileheight="64" tilecount="42" columns="2">
- <image source="../frontend/src/data/img/spritesheets/Trees.png" width="128" height="1344"/>
+ <image source="../img/spritesheets/Trees.png" width="128" height="1344"/>
 </tileset>
