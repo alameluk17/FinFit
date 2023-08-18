14ec2e3beb4f63f6d3f1750e1b433fc33a3e0021
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-08-18 14:24:55 +0530

    Rename "Assets" folder to "spritesheets" folder.

--- TiledSpecifics/House.tsx
+++ TiledSpecifics/House.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="House" tilewidth="64" tileheight="64" tilecount="124" columns="4">
- <image source="../Assets/House.png" width="256" height="1984"/>
+ <image source="../spritesheets/House.png" width="256" height="1984"/>
 </tileset>
--- TiledSpecifics/Shops.tsx
+++ TiledSpecifics/Shops.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Shops" tilewidth="64" tileheight="64" tilecount="66" columns="22">
- <image source="../Assets/Shops.png" width="1408" height="192"/>
+ <image source="../spritesheets/Shops.png" width="1408" height="192"/>
 </tileset>
