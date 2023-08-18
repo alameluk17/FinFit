61aa2bf1ec5964551e295b68e513623afbc8ed0a
alameluk17 <alamukanna14@gmail.com>
2023-08-18 17:30:34 +0530

    Tried loading assets

--- frontend/src/manifest.js
+++ frontend/src/manifest.js
@@ -18,6 +18,57 @@ const DataManifest = [
         type: "tmx",
         src: "./data/map/finfit.tmx"
     },
+    {
+        name: "CityHall_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/CityHall.tsx"
+    },
+    {
+        name: "FoodAndCars_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/FoodAndCars.tsx"
+    },
+    {
+        name: "Grass_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/Grass.tsx"
+    },
+    {
+        name: "House_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/House.tsx"
+    },
+    {
+        name: "OfflBuildings_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/OfflBuildings.tsx"
+    },
+    {
+        name: "Park_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/Park.tsx"
+    },
+    {
+        name: "RoadTiles_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/RoadTiles.tsx"
+    },
+    {
+        name: "Shops_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/Shops.tsx"
+    },
+    {
+        name: "SpecialBuildings_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/SpecialBuildings.tsx"
+    },
+    {
+        name: "Trees_spritesheet",
+        type: "tsx",
+        src: "./data/tilesets/Trees.tsx"
+    }
+   
 ];
 
 export default DataManifest;
