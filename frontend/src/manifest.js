4dc9b95442ff06860e219d9829c9ffb6bcf5f88b
Aathish Sivasubrahmanian <aathish04@gmail.com>
2023-08-19 12:34:30 +0530

    Import all tilesets and maps appropriately.

--- frontend/src/manifest.js
+++ frontend/src/manifest.js
@@ -13,11 +13,115 @@ const DataManifest = [
         type: "binary",
         src: "./data/fnt/PressStart2P.fnt"
     },
+    // Map File
     {
         name: "finfit_map",
         type: "tmx",
         src: "./data/map/finfit.tmx"
     },
+
+    // Tilesets
+    {
+        name: "CityHall",
+        type: "tsx",
+        src: "./data/tilesets/CityHall.tsx"
+    },
+    {
+        name: "FoodAndCars",
+        type: "tsx",
+        src: "./data/tilesets/FoodAndCars.tsx"
+    },
+    {
+        name: "Grass",
+        type: "tsx",
+        src: "./data/tilesets/Grass.tsx"
+    },
+    {
+        name: "House",
+        type: "tsx",
+        src: "./data/tilesets/House.tsx"
+    },
+    {
+        name: "OfflBuildings",
+        type: "tsx",
+        src: "./data/tilesets/OfflBuildings.tsx"
+    },
+    {
+        name: "Park",
+        type: "tsx",
+        src: "./data/tilesets/Park.tsx"
+    },
+    {
+        name: "RoadTiles",
+        type: "tsx",
+        src: "./data/tilesets/RoadTiles.tsx"
+    },
+    {
+        name: "Shops",
+        type: "tsx",
+        src: "./data/tilesets/Shops.tsx"
+    },
+    {
+        name: "SpecialBuildings",
+        type: "tsx",
+        src: "./data/tilesets/SpecialBuildings.tsx"
+    },
+    {
+        name: "Trees",
+        type: "tsx",
+        src: "./data/tilesets/Trees.tsx"
+    },
+    // Spritesheets
+    {
+        name: "CityHall",
+        type: "image",
+        src:  "./data/img/spritesheets/CityHall.png"
+    },
+    {
+        name: "FoodAndCars",
+        type: "image",
+        src:  "./data/img/spritesheets/FoodAndCars.png"
+    },
+    {
+        name: "Grass",
+        type: "image",
+        src:  "./data/img/spritesheets/Grass.png"
+    },
+    {
+        name: "House",
+        type: "image",
+        src:  "./data/img/spritesheets/House.png"
+    },
+    {
+        name: "OfflBuildings",
+        type: "image",
+        src:  "./data/img/spritesheets/OfflBuildings.png"
+    },
+    {
+        name: "Park",
+        type: "image",
+        src:  "./data/img/spritesheets/Park.png"
+    },
+    {
+        name: "RoadTiles",
+        type: "image",
+        src:  "./data/img/spritesheets/RoadTiles.png"
+    },
+    {
+        name: "Shops",
+        type: "image",
+        src:  "./data/img/spritesheets/Shops.png"
+    },
+    {
+        name: "SpecialBuildings",
+        type: "image",
+        src:  "./data/img/spritesheets/SpecialBuildings.png"
+    },
+    {
+        name: "Trees",
+        type: "image",
+        src:  "./data/img/spritesheets/Trees.png"
+    }
 ];
 
 export default DataManifest;
